31bf84939d758d3d5a8996657e97b1b2beeebaec
Marc Batete <ryanebengo@gmail.com>
2019-11-12 20:01:23 -0800

    Initial Commit

---
+++ HotelManzana2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotelManzana2
+//
+//  Created by Marc Batete on 11/12/19.
+//  Copyright © 2019 Marc Batete. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
